da3547a51705d2d5200528fc3d845ca2c0bb4e6f
Daniela <dnuno2007@gmail.com>
2026-05-28 14:19:46 -0600

    revive warrior

--- warrior.cpp
+++ warrior.cpp
@@ -39,6 +39,31 @@ void Warrior::attack(CombatUnit &target){
     target.receiveAttack(finalDamage);
 }
 
+void Warrior::revive(){
+    /*A combatant will not only be considered dead when losing all life points, but this will also depend on the specialization 
+    attribute of the derived classes. Therefore, this method will not be implemented in the base class, and being pure virtual, 
+    it will make the base class abstract. This method must be implemented in each derived class.
+
+    Add the revive() method to your derived classes. As mentioned above, this method should not depend solely on the current life 
+    points, but also on at least one specialized attribute of the object. For example, for a Mage whose attribute is mana: if the 
+    Mage loses all health but still has sufficient mana, some life points may be restored and the character will not die. You may 
+    display a message such as “The character revived due to mana” and reduce the mana value (so that this ability can only be used 
+    once or twice at most). If the Mage does not have sufficient mana, then the health remains at 0 and a message indicating that the
+    character has died should be displayed. Update the receiveAttack methods in the derived classes to use revive(). This should 
+    be done after updating the health points. By doing so, you will need to override and ensure runtime polymorphic behavior. */
+
+    // for the warrior, they could revive by using their strength to survive a fatal attack.
+    if (get_health() <= 0 && strength >= 20) { // if the warrior has at least 20 strength, they can revive
+        set_health(get_life() / 2); // restore half of the life points
+        strength -= 20; // reduce strength by 20
+        cout << type << " revived using strength! Current health: " << get_health() << ", Remaining strength: " << strength << endl;
+    } else if (get_health() <= 0) {
+        set_health(0);
+        cout << type << " has died." << endl;
+    }
+    
+}
+
 void Warrior::receiveAttack(int attackPoints){
     int mitigatedDamage = attackPoints - this->strength;
 
