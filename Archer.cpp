6fe829b5a4a728f90a3c34b99e64d5a7fe691729
Daniela <dnuno2007@gmail.com>
2026-05-28 14:18:40 -0600

    revive archer

--- Archer.cpp
+++ Archer.cpp
@@ -34,6 +34,31 @@ void Archer::setAgility(int agility){
     this->agility = agility;
 }
 
+void Archer::revive(){
+    /*A combatant will not only be considered dead when losing all life points, but this will also depend on the specialization 
+    attribute of the derived classes. Therefore, this method will not be implemented in the base class, and being pure virtual, 
+    it will make the base class abstract. This method must be implemented in each derived class.
+
+    Add the revive() method to your derived classes. As mentioned above, this method should not depend solely on the current life 
+    points, but also on at least one specialized attribute of the object. For example, for a Mage whose attribute is mana: if the 
+    Mage loses all health but still has sufficient mana, some life points may be restored and the character will not die. You may 
+    display a message such as “The character revived due to mana” and reduce the mana value (so that this ability can only be used 
+    once or twice at most). If the Mage does not have sufficient mana, then the health remains at 0 and a message indicating that the
+    character has died should be displayed. Update the receiveAttack methods in the derived classes to use revive(). This should 
+    be done after updating the health points. By doing so, you will need to override and ensure runtime polymorphic behavior. */
+
+    // for the archer, they could revive by using agility to dodge a fatal attack.
+    if (get_health() <= 0 && agility >= 50) { // if the archer has at least 50 agility, they can revive
+        set_health(get_life() / 2); // restore half of the life points
+        agility -= 50; // reduce agility by 50
+        cout << type << " revived using agility! Current health: " << get_health() << ", Remaining agility: " << agility << endl;
+    } else if (get_health() <= 0) {
+        set_health(0);
+        cout << type << " has died." << endl;
+    }
+
+}
+
 void Archer::receiveAttack(int attackPoints) {
     /* Derived classes must override the print(), receiveAttack(), and attack() methods. Below are some suggestions for how
      the methods might vary for one of the classes (you will need to decide how they will be modified for each derived class).*/
@@ -51,6 +76,8 @@ void Archer::receiveAttack(int attackPoints) {
         }
         cout << type << " received " << attackPoints << " damage." << endl << endl;
     }
+
+    revive(); // check if the archer can revive after receiving damage
 
 }
 
